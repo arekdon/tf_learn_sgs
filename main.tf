6efc17a453d7c77f06834e5a904378fd2afae27b
Arek Donajski <arkadiusz.donajski@contracted.pmi.com>
2019-11-21 17:36:32 +0100

    list instead of string

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "v2lbsg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -16,7 +16,7 @@ resource "aws_security_group" "v2lbsg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
